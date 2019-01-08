91e89c5bd030d8fd2738755ea823a3e0c9c9732c
halsos <oddih8@gmail.com>
2019-01-08 21:21:35 +0100

    C thread with for-loop

--- Part4/c/foo.c
+++ Part4/c/foo.c
@@ -6,23 +6,34 @@ int i = 0;
 // Note the return type: void*
 void* incrementingThreadFunction(){
     // TODO: increment i 1_000_000 times
+    for ( int j = 0; j < 1000000; j++){
+        i++;
+        printf("thread1\n");
+    }
     return NULL;
 }
 
 void* decrementingThreadFunction(){
     // TODO: decrement i 1_000_000 times
+    for (int k = 0; k < 1000000; k++){
+        i--;
+        printf("thread2\n");
+    }
     return NULL;
 }
 
 
 int main(){
     // TODO: declare incrementingThread and decrementingThread (hint: google pthread_create)
+    pthread_t incrementingThread;
+    pthread_t decrementingThread;
+
     pthread_create(&incrementingThread, NULL, incrementingThreadFunction, NULL);
     pthread_create(&decrementingThread, NULL, decrementingThreadFunction, NULL);
-    
+
     pthread_join(incrementingThread, NULL);
     pthread_join(decrementingThread, NULL);
-    
+
     printf("The magic number is: %d\n", i);
     return 0;
 }
